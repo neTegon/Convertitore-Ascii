d6fdc5b9d568b277cbbc84d676f2ca6c7e6b30f3
Elettra Tegon <112578860+neTegon@users.noreply.github.com>
2024-03-03 15:37:05 +0100

    Add files via upload

---
+++ CodiceAscii/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+StreamReader fs = new StreamReader("fileSorgente.txt");
+StreamWriter fd = new StreamWriter("fileCopia.txt");
+string riga = string.Empty;
+while (!fs.EndOfStream)
+{
+    riga = fs.ReadLine();
+    byte[] asciiBytes = Encoding.ASCII.GetBytes(riga);
+    foreach(byte b in asciiBytes) 
+    {
+        fd.Write(b);
+    }
+    fd.WriteLine();
+}
+fd.Close();      
+    
